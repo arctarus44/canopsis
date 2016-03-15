8650c61c86bec3a7e9b3c7a973758ba9c9424798
tgosselin <tgosselin@debian>
2016-03-15 11:10:56 +0100

    what i did for meeting

--- sources/python/bench/canopsis/bench/manager.py
+++ sources/python/bench/canopsis/bench/manager.py
@@ -1,9 +1,16 @@
+# -*- coding: utf-8 -*-
+
 from canopsis.middleware.registry import MiddlewareRegistery
 
 CONF_PATh = 'bench/manager.conf'
+CATEGORY = 'BENCH'
 
 
 class Bench(MiddlewareRegistery):
     """
     bench storage
     """
+
+    NAME = 'bench'
+
+    STORAGE = 'bench_storage'
