45d5ac6be32aea006be7674d06f09e8d7da15101
tgosselin <contact@dowan.beer>
2016-09-30 18:03:52 +0200

    starting baseline

---
+++ sources/python/baseline/setup.py
@@ -0,0 +1,9 @@
+from canopsis.common.setup import setup
+
+install_requires = ['canopsis.engines']
+
+setup(
+    description='base line engine package',
+    install_requires=install_requires,
+    keywords='baseline'
+)
