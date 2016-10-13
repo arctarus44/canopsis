5179f06bfaee37a3624a49f0ac97a35e72166379
tgosselin <contact@dowan.beer>
2016-10-13 17:48:18 +0200

    reset + static

--- sources/python/baseline/canopsis/baseline/manager.py
+++ sources/python/baseline/canopsis/baseline/manager.py
@@ -27,7 +27,6 @@ class Baseline(MiddlewareRegistry):
         :param **kwargs:
         """
         super(Baseline, self).__init__(*args, **kwargs)
-        self.check = True
 
     def get_baselines(self, baseline_name, timewindow=None):
         """get_baselines"""
@@ -38,7 +37,9 @@ class Baseline(MiddlewareRegistry):
         else:
             _timewindow = timewindow
 
-        return self[Baseline.STORAGE].get(data_id=baseline_name, timewindow=_timewindow)
+        return self[Baseline.STORAGE].get(
+            data_id=baseline_name,
+            timewindow=_timewindow)
 
     def put(self, name, value):
         point = (time(), value)
@@ -64,75 +65,80 @@ class Baseline(MiddlewareRegistry):
             'resource': resource
         }
 
-        if self.check:
-            if not len(self[Baseline.CONFSTORAGE].get_elements(query={'_id':'baselines'})) > 0:
-                self[Baseline.CONFSTORAGE].put_element({'_id':'baselines', 'list':[]})
-                self.check = False
+        self.manage_baselines_list(element)
+
+        return self[Baseline.CONFSTORAGE].put_element(
+            element,
+            _id=baseline_name)
+
+    def manage_baselines_list(self, element):
+
+        if not len(self[Baseline.CONFSTORAGE].get_elements(query={'_id': 'baselines'})) > 0:
+            self[Baseline.CONFSTORAGE].put_element(
+                {'_id': 'baselines', 'list': []})
 
         baselines = {}
-        for i in self[Baseline.CONFSTORAGE].get_elements(query={'_id':'baselines'}):
+        for i in self[Baseline.CONFSTORAGE].get_elements(query={'_id': 'baselines'}):
             baselines = i
 
         l = baselines['list']
-        l.append({baseline_name: time() + period})
-        #revoir par ce que la on écrase pas mais on ajoute dans la liste bref c'est de la merde'
+        index = -1
+        for k, i in enumerate(l):
+            if element['baseline_name'] in i:
+                index = k
+        if index == -1:
+            l.append({element['baseline_name']: time() + element['period']})
+        else:
+            l[index][element['baseline_name']] = time() + element['period']
+
         baselines['list'] = l
         self[Baseline.CONFSTORAGE].put_element(baselines)
-        return self[Baseline.CONFSTORAGE].put_element(element, _id=baseline_name)
 
     def beat(self):
         now = time()
         baselines = {}
-        for i in self[Baseline.CONFSTORAGE].get_elements(query={'_id':'baselines'}):
+        for i in self[Baseline.CONFSTORAGE].get_elements(query={'_id': 'baselines'}):
             baselines = i
         baseline_list = baselines['list']
 
         for i in baseline_list:
             if now > i.items()[0][1]:
                 self.logger.error('voila on est bon on doit checker')
-        #continuer ici/revoir le check pour ensuite passer a check baseline et on sera pas mal
+                self.reset_timestamp(i.items()[0][0])
+                self.check_baseline(i.items()[0][0], i.items()[0][1])
 
+    def reset_timestamp(self, baseline_name):
+        self.logger.error('reset_timestamp param: {0}\n'.format(baseline_name))
+        baseline = {}
+        for i in self[Baseline.CONFSTORAGE].get_elements(query={'_id': baseline_name}):
+            baseline = i
+        period = baseline['period']
 
+        baselines = {}
+        for i in self[Baseline.CONFSTORAGE].get_elements(query={'_id': 'baselines'}):
+            baselines = i
+        baseline_list = baselines['list']
 
+        index = -1
+        for k, i in enumerate(baseline_list):
+            if baseline_name in i:
+                index = k
 
-        """
-        for baseline_name, timestamp in self.baselines.items():
-            f = open('/home/tgosselin/fichierdelog3', 'a')
-            f.write('for en beat\n')
-            f.close()
-            if now > timestamp:
-                f = open('/home/tgosselin/fichierdelog3', 'a')
-                f.write('on check !\n')
-                f.close()
-                self.check_baseline(baseline_name, timestamp)
-            else:
-                f = open('/home/tgosselin/fichierdelog3', 'a')
-                f.write('on check pas encore \n')
-                f.close()
-                f = open('/home/tgosselin/fichierdelog3', 'a')
-                f.write('fin du beat manager\n')
-                f.close()
-        """
+        baseline_list[index][baseline_name] = baseline_list[index][baseline_name] + period
+        baselines['list'] = baseline_list
+        self[Baseline.CONFSTORAGE].put_element(baselines)
+
     def check_baseline(self, baseline_name, timestamp):
-        f = open('/home/tgosselin/fichierdelog3', 'a')
-        f.write('check baseline\n')
-        f.close()
-        conf = self[Baseline.CONFSTORAGE].get_elements(
-            query={"_id": baseline_name})
-        baselineconf = {}
+        #get baseline config in db
+        baseline_conf = {}
+        for i in self[Baseline.CONFSTORAGE].get_elements(query={'_id': baseline_name}):
+            baseline_conf = i
 
-        for i in conf:
-            baselineconf = i
+        if baseline_conf['mode'] ==  'static':
+            tw = TimeWindow(start=timestamp - baseline_conf['period'], stop=timestamp)
+            self.logger.error(self.get_baselines(baseline_name, tw))
+        else:
+            pass
+        #faire les truc autre que static
 
-        timewindow = TimeWindow(
-            start=timestamp - conf['period'], stop=timestamp)
-        """
-        result = self.get_baselines(baseline_name, timewindow)
-
-        f = open('/home/tgosselin/fichierdelog3', 'a')
-        f.write('a{0}\n'.format(result))
-        f.close()
-        """
-    # chopper la config de la baseline a checker dans mongo donc un petit get qui va bien
-    # faire la requete des métrique a chopper en fonction de la config travailler le get avec la time window
     # déclencher l'alarme remettre le compteur a jour etc
